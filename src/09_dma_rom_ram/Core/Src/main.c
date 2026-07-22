d673d4f3328b66187b4bca869772b8e92eaf5e14
luke <luke123@foxmail.com>
2026-07-22 12:01:34 +0800

    feat(project): 初始化 DMA ROM 到 RAM 传输项目配置
    - 添加 STM32F103ZET6 微控制器的 DMA 配置文件 (09_dma_rom_ram.ioc)
    - 集成 CMSIS 头文件 cmsis_armcc.h 以支持 ARM 编译器
    - 创建 CMake 构建系统配置文件 CMakeLists.txt 和 cmake/stm32cubemx/CMakeLists.txt
    - 配置 CMake 预设构建选项 CMakePresets.json
    - 设置 DMA 内存到内存传输参数和相关中断向量
    - 定义项目构建工具链和编译选项

---
+++ src/09_dma_rom_ram/Core/Src/main.c
@@ -0,0 +1,257 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+#include <stdio.h>
+
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+const uint8_t data[] = {11, 12, 13, 14};
+
+uint8_t dest[] = {0};
+
+// 传输结束标记
+uint8_t flag = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+void dma1_full_transfer_callback(DMA_HandleTypeDef* dma_handle_type_def);
+void dma1_transfer_callback(DMA_HandleTypeDef* dma_handle_type_def);
+void dma1_error_callback(DMA_HandleTypeDef* dma_handle_type_def);
+void dma1_abort_callback(DMA_HandleTypeDef* dma_handle_type_def);
+void dma1_all_callback(DMA_HandleTypeDef* dma_handle_type_def);
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  // 开始
+  printf("DMA Start\r\n");
+
+  // 开启DMA传输
+  HAL_DMA_Start_IT(
+    // 源
+    &hdma_memtomem_dma1_channel1,
+    // 目标
+    (uint32_t)data,
+    // 源
+    (uint32_t)dest,
+    // 长度
+    4
+  );
+
+  // 使用注册回调函数实现
+  HAL_DMA_RegisterCallback(&hdma_memtomem_dma1_channel1, HAL_DMA_XFER_CPLT_CB_ID, &dma1_full_transfer_callback);
+  HAL_DMA_RegisterCallback(&hdma_memtomem_dma1_channel1, HAL_DMA_XFER_HALFCPLT_CB_ID, &dma1_transfer_callback);
+  HAL_DMA_RegisterCallback(&hdma_memtomem_dma1_channel1, HAL_DMA_XFER_ERROR_CB_ID, &dma1_error_callback);
+  HAL_DMA_RegisterCallback(&hdma_memtomem_dma1_channel1, HAL_DMA_XFER_ABORT_CB_ID, &dma1_abort_callback);
+  HAL_DMA_RegisterCallback(&hdma_memtomem_dma1_channel1, HAL_DMA_XFER_ALL_CB_ID, &dma1_all_callback);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+    if (flag)
+    {
+      printf("DMA End\r\n");
+
+      for (uint8_t i = 0; i < 4; i++)
+      {
+        printf("dest[%d] = %d\r\n", i, dest[i]);
+      }
+      flag = 0;
+    }
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+    | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+// DMA 全传输完成回调
+void dma1_full_transfer_callback(DMA_HandleTypeDef* hdma)
+{
+  printf("DMA Full Transfer Complete\r\n");
+  flag = 1;
+}
+
+// DMA 半传输完成回调
+void dma1_transfer_callback(DMA_HandleTypeDef* hdma)
+{
+  printf("DMA Half Transfer Complete\r\n");
+}
+
+// DMA 错误回调
+void dma1_error_callback(DMA_HandleTypeDef* hdma)
+{
+  printf("DMA Transfer Error\r\n");
+}
+
+// DMA 中止回调
+void dma1_abort_callback(DMA_HandleTypeDef* hdma)
+{
+  printf("DMA Transfer Aborted\r\n");
+}
+
+// DMA 所有回调（在其他回调之前调用）
+void dma1_all_callback(DMA_HandleTypeDef* hdma)
+{
+  // 此回调在所有其他回调之前调用，可用于统一处理
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
